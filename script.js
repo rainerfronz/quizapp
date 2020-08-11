726cd02e37b4de83ae13f69d0734826c0d652b4a
Rainer J Fronz <rainerfronz44@gmail.com>
2020-08-10 22:21:27 -0500

    move thru questions.
    brings up new question after submiting answer,
    correrct and incorrect/score not functioning properly.
    when trying to update question count everything goes away.

--- script.js
+++ script.js
@@ -1,109 +1,141 @@
 const STORE = {
-  questionNumber:0,
-  score:0,
-  view:'landing',
+  questionNumber: 0,
+  score: 0,
+  view: 'landing',
 
 
   questions: [
     {
-title: 'How Many States are in US?',
-answers: ['2', '50', '13', '52'],
-correct: '50'
-},
-   {
-    title: 'What year was the Constitution signed?',
-    answers: ['1984', '1776', '2001', '1819'],
-    correct: '1776'
-   },
-   { 
-     title: 'Which president was famous for Fireside Chats, the New Deal and ending WWII?',
-   answers: ['Franklin Roosevelt', 'William Taft' , 'Harry Truman', 'Jimmy Carter'],
-   correct: 'Franklin Roosevelt'
-   },
-   {
-     title: 'Which Direction is Detroit, MI from Windsor, ONT Canada?',
-     answers: ['South', 'West', 'East' , 'North'],
-     correct: 'North'
-   },
-   {
-     title: 'Which leader passed the Civil Righs Act?',
-   answers: ['Martin Luther', 'Martin Luther King Jr.' , 'Lyndon B Johnson', 'Donald Trump'],
-   correct: 'Lyndon B Johnson'
-   },
-   {
-     title: 'Who is the current Vice President?',
-     answers: ['Dick Cheney', 'William Barr','Mike Pence', 'Jared Kushner'],
-     correct: 'Mike Pence'
-   },
-  {
-    title: 'When was the Bill of Rights signed?',
-    answers: ['1976', '1791', '1689', '2001'],
-    correct: '1791'
+      title: 'How Many States are in US?',
+      answers: ['2', '50', '13', '52'],
+      correct: 1
+    },
+    {
+      title: 'What year was the Constitution signed?',
+      answers: ['1984', '1776', '2001', '1819'],
+      correct: '1776'
+    },
+    {
+      title: 'Which president was famous for Fireside Chats, the New Deal and ending WWII?',
+      answers: ['Franklin Roosevelt', 'William Taft', 'Harry Truman', 'Jimmy Carter'],
+      correct: 'Franklin Roosevelt'
+    },
+    {
+      title: 'Which Direction is Detroit, MI from Windsor, ONT Canada?',
+      answers: ['South', 'West', 'East', 'North'],
+      correct: 'North'
+    },
+    {
+      title: 'Which leader passed the Civil Righs Act?',
+      answers: ['Martin Luther', 'Martin Luther King Jr.', 'Lyndon B Johnson', 'Donald Trump'],
+      correct: 'Lyndon B Johnson'
+    },
+    {
+      title: 'Who is the current Vice President?',
+      answers: ['Dick Cheney', 'William Barr', 'Mike Pence', 'Jared Kushner'],
+      correct: 'Mike Pence'
+    },
+    {
+      title: 'When was the Bill of Rights signed?',
+      answers: ['1976', '1791', '1689', '2001'],
+      correct: '1791'
     },
     {
       title: 'Which Amendment expaned voter rights?',
-      answers:['10','24', '5', '15'],
+      answers: ['10', '24', '5', '15'],
       correct: '15'
     },
     {
       title: 'How many Moutain Ranges are in USA?',
-      answers:['5','3', '2', '1'],
+      answers: ['5', '3', '2', '1'],
       correct: '3'
     },
     {
-      title:'What determines election day?',
-      answers:['straw poll', 'Voted on by Senate', 'Tuesday next after first Monday in November', 'Primary Vote'],
-      correct:'Tuesday next after first Monday'
+      title: 'What determines election day?',
+      answers: ['straw poll', 'Voted on by Senate', 'Tuesday next after first Monday in November', 'Primary Vote'],
+      correct: 'Tuesday next after first Monday'
     },
   ]
 }
 
 /* template for question function in dom*/
 function renderQuiz() {
-  console.log('questions');
+  $('#start').on('click', function (event) {
+    STORE.page = "question"
+    render();
+    console.log('click')
+  });
 }
 
 
+function render() {
 
-/*score update function, increase by 1*/
-function updateScore(){}
-/*resets values of score and question number*/
-
-/*increase num of question count on page view. again +1*/
-function updateQuestion(){}
-
-/*start quiz function*/
-function startQuiz(){}
-/*checks answers*/
-function checkAnswer(){}
+  let question = STORE.questions[STORE.questionNumber]
+  if (STORE.page == "question") {
 
 
-/*create question  html pages*/
-function makeQuestionPage(){}
+    $(`#content`).html(`
+   <form id="#js-title">
+   
+    <legend>${question.title}</legend>
+    <label>${question.answers[0]}</label>
+    <input type="radio" name="answer" value= "0"/>
+    <label>${question.answers[1]}</label>
+    <input type="radio" name="answer" value= "1"/>
+    <label>${question.answers[2]}</label>
+    <input type="radio" name="answer" value= "2"/>
+    <label>${question.answers[3]}</label>
+    <input type="radio" name="answer" value= "3"/>
+    <section class="row">
+      <button type = "submit" id="answer">Submit</button>
+    </section>
+  </form>
+  `)
+  }
+
+}
+function answerSubmit() {
+  $("body").on(`submit`, `form`, e => {
+    e.preventDefault();
+
+    const answer = e.target.answer.value;
+    console.log("answer: " + answer);
+    if (answer.value === STORE.questions[STORE.questions.correct]) {
+
+      console.log('answer is correct');
+      STORE.score += 1;
+      console.log('score' + STORE.score);
+      $('#score').text(STORE.score);
+      // update score in DOM
+    } else {
+      // handle wrong answer
+    }
+
+    STORE.questionNumber++;
+    //$('#questionsCount').text(STORE.questionNumber);{
+
+    //}
+
+    console.log("question: " + STORE.questionNumber);
+    //next button
+    render();
+
+
+  })
+}
 
 /*correct answer does this*/
-function correctAnswer(){}
+function correctAnswer() { }
 
 /*Wrong Answer does this */
-function wrongAnswer(){}
-/*finds next question*/
-function nextQuestion(){}
+function wrongAnswer() { }
 
-/*adds up score*/
-function finalScore(){}
+
 /*restart quiz button*/
-function restartQuiz(){}
+function restartQuiz() { }
 
-function handleQuiz()
-{
+function handleQuiz() {
   renderQuiz()
-  updateScore()
-  updateQuestionNumber()
-  startQuiz()
-  checkAnswer()
-  nextQuestion()
-  restartQuiz()
-
-
+  answerSubmit()
 }
 $(handleQuiz)
